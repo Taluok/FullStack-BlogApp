f0483e1a6ecb1127feaff16ea1edf06b4631e75c
Taluok <tanialuduenaok@gmail.com>
2024-08-27 13:17:08 -0300

    Create blog list page for adm

--- blogapp/app/admin/blogList/page.jsx
+++ blogapp/app/admin/blogList/page.jsx
@@ -1,10 +1,50 @@
 'use client'
-import React from 'react'
+import BlogTableItem from '@/Components/AdminComponent/BlogTableItem'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
-    return (
-        <div>
 
+    const [blogs,setBlogs] = useState([]);
+
+    const fetchBlogs = async () => {
+        const response = await axios.get('/api/blog');
+        setBlogs(response.data.blogs)
+    }
+
+    useEffect(()=>{
+        fetchBlogs()
+    },[])
+
+    return (
+        <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+            <h1>All blogs</h1>
+            <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide">
+                <table className='w-full text-sm text-gray-500'>
+                    <thead className='text-sm text text-gray-700 text-left uppercase bg-gray-50'>
+                        <tr>
+                            <th scope='col'className='hidden sm:block px6 py-3'>
+                                Nombre del autor
+                            </th>
+                            <th scope='col'className='px6 py-3'>
+                                Titulo del Blog
+                            </th>
+                            <th scope='col'className='px6 py-3'>
+                                Fecha del Blog
+                            </th>
+                            <th scope='col'className='px6 py-3'>
+                                Accion
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {blogs.map((item,index)=>{
+                            return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date}/>
+                        })}
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
