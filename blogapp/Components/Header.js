07fb40dd3596d8d6a43bc42ed6a7752c2f385b6c
Taluok <tanialuduenaok@gmail.com>
2024-08-29 11:57:49 -0300

    create email subscription

--- blogapp/Components/Header.js
+++ blogapp/Components/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Image from 'next/image';
 import { assets } from '@/Assets/assets';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 
 const Header = () => {
+
+    const [email, setEmail] = useState("");
+    const onSubmitHandler = async () => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("email", email);
+        const response = await axios.post('/api/email',formData);
+        if (response.data.success) {
+            toast.success(response.data.msg);
+            setEmail("");
+        }
+        else{
+            toast.error("Error")
+        }
+    }
+
     return (
         <div className='py-5 px-5 md:px-12 lg:px-28'>
             <div className='flex justify-between items-center'>
@@ -25,6 +43,8 @@ const Header = () => {
                 </p>
                 <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black' style={{ boxShadow: '-7px 7px 0px #000000' }} action="">
                     <input
+                        onChange={(e)=>setEmail(e.target.value)}
+                        value={email}
                         type='email'
                         placeholder='Ingresa tu correo electrónico'
                         className='pl-4 outline-none flex-grow'
---
+++ blogapp/app/api/email/route.js
@@ -0,0 +1,16 @@
+import EmailModel from "@/lib/models/EmailModel";
+import { NextResponse } from "next/server";
+
+const LoadDB = async () => {
+    await ConnectDB()
+}
+loadDB();
+
+export async function POST(request){
+    const formData = await request.formData();
+    const emailData = {
+        email:`${formData.get('email')}`
+    }
+    await EmailModel.create(emailData);
+    return NextResponse.json({success:true,msg:"Email Subscripto"})
+}
